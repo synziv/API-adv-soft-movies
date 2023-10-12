479a517e427e294a63900874e050b405198c6fd6
Alexis Brouillette <alexisbrou1@hotmail.fr>
2023-10-11 21:08:34 -0400

    adds invalide route testing

--- src/tests/user.test.ts
+++ src/tests/user.test.ts
@@ -3,7 +3,7 @@ import { app } from "..";
 
 //this test is an exemple of how to test a route
 //it should be modified when we have the logoff route implemented
-describe("User testing", () => {
+describe("Valid User testing", () => {
   test("POST /user/logoff", async () => {
     const user = "John Doe";
     const req = new Request("http://localhost/user/logoff", {
@@ -13,5 +13,22 @@ describe("User testing", () => {
     const res = await app.request(req);
     expect(res.status).toBe(200);
     expect(await res.text()).toBe(`User ${user} logged off from backend`);
+  });
+});
+
+// test for invalide route
+describe("Invalid route", () => {
+  test("POST /user/logoff", async () => {
+    
+    const req = new Request("http://localhost/unvalid-route", {
+      method: "POST",
+    });
+    
+    const res = await app.request(req);
+    const {message, ok} = await res.json();
+
+    expect(res.status).toBe(404);
+    expect(message).toBe(`Not Found`);
+    expect(ok).toBe(false);
   });
 });
