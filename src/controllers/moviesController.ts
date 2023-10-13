6434c3a790c064f5cc4fd640b816d58d4e6a067f
Alexis Brouillette <alexisbrou1@hotmail.fr>
2023-10-12 22:47:28 -0400

    adds wish list request and controller
    TODO firebase linking with user

--- src/controllers/moviesController.ts
+++ src/controllers/moviesController.ts
@@ -3,3 +3,15 @@ import { Context, Env } from "hono";
 export const getMovies = async (c: Context<Env, "/", {}>) => {
     return c.json({ name: "John Doe" });
 };
+
+export const addToWishList = async (c: Context<Env, "/addToWishlist", {}>) => {
+    const body =  await c.req.json();
+    const movieId = body
+
+    //TODO add movie to wishlist in firebase. If movie already exists, remove it from wishlist
+
+    
+
+    //added or removed
+    return c.json({ status: "added" });
+}
--- src/routes/movies.ts
+++ src/routes/movies.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
-import { getMovies } from "../controllers/moviesController";
+import { addToWishList, getMovies } from "../controllers/moviesController";
 
 export const moviesRouter = new Hono()
 
 moviesRouter.get("/", getMovies)
+moviesRouter.post("/addToWishlist",addToWishList);
